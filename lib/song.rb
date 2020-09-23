b1f2d747d1d771b1ec02e9971a92ff384b711b51
danny galimi <dannygalimi13@aim.com>
2020-09-23 21:10:42 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+class Song
+  
+@@count = 0
+@@artists = []
+  
+attr_accessor :name, :artist, :genre
+  
+def initialize(name,artist,genre)
+  @name = name
+  @artist = artist
+  @genre = genre
+  @@count += 1
+end
+  
+end
