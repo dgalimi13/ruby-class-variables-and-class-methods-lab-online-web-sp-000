282165f8167bfdaa8308160caf1849d51738841f
danny galimi <dannygalimi13@aim.com>
2020-09-23 21:26:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,7 @@ attr_accessor :name, :artist, :genre
   
 def initialize(name,artist,genre)
   @name = name
-  @artist = artist
+  @artist = artist.to_a
   @genre = genre
   @@count += 1
   @artist << @@artists
